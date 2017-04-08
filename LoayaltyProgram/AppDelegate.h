ecf80ee0341278f9d815ab4faf8049cac1a3b15a
Bogdan Chaikovsky <bogdan@Users-MacBook-Pro.local>
2017-04-08 15:53:25 +0300

    Initial Commit

---
+++ LoayaltyProgram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoayaltyProgram
+//
+//  Created by Bogdan Chaikovsky on 08.04.17.
+//  Copyright © 2017 Bogdan Chaikovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoayaltyProgram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoayaltyProgram
+//
+//  Created by Bogdan Chaikovsky on 08.04.17.
+//  Copyright © 2017 Bogdan Chaikovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

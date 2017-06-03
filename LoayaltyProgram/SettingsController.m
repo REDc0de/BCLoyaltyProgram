08148f9a1c2d106c2cf2e289fb3c639638fb00f0
Bogdan Chaikovsky <bogdan.chaikovsky@belprog.com>
2017-06-03 14:01:22 +0300

    Settings 0.6

--- LoayaltyProgram/SettingsController.m
+++ LoayaltyProgram/SettingsController.m
@@ -9,14 +9,16 @@
 #import "SettingsController.h"
 #import <MapKit/MapKit.h>
 #import "UIViewController+Alerts.h"
+#import <MessageUI/MessageUI.h>
 
-@interface SettingsController () <CLLocationManagerDelegate>
+@interface SettingsController () <CLLocationManagerDelegate, MFMailComposeViewControllerDelegate>
 @property (strong, nonatomic) CLLocationManager *locationManager;
 @property (weak, nonatomic) IBOutlet UILabel *locationStatusLabel;
 
 @end
 
 @implementation SettingsController
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -29,19 +31,37 @@
     [super viewWillAppear:animated];
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.section == 1 && indexPath.row == 0) {
+        // The user want to setup location.
+        [self setupLocationPremission];
+    }
+    else if (indexPath.section == 2 && indexPath.row == 1) {
+        // The user want to send feedback.
+        NSString *subject = @"Loyalty Program Feedback";
+        NSString *message = [NSString stringWithFormat:@"Device: %@\n System: %@ %@", [[UIDevice currentDevice] model], [[UIDevice currentDevice] systemName], [[UIDevice currentDevice] systemVersion]];
+        NSString *recipient = @"infinityblade11@gmail.com";
+        [self sendMailWithSubject:subject message:message toRecipient:recipient];
+    }
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+
+#pragma mark - Location Manager Delegate
+
 - (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status{
     
     switch (status) {
         case 0:
             // The user has not yet made a choice regarding whether this app can use location services.
-           self.locationStatusLabel.text = @"Not determined";
+            self.locationStatusLabel.text = @"Not determined";
             break;
         case 1:
             //This app is not authorized to use location services. The user cannot change this app’s status, possibly due to active restrictions such as parental controls being in place.
             self.locationStatusLabel.text = @"Restricted";
             break;
         case 2:
-           // The user explicitly denied the use of location services for this app or location services are currently disabled in Settings.
+            // The user explicitly denied the use of location services for this app or location services are currently disabled in Settings.
             if ([CLLocationManager locationServicesEnabled]){
                 // The user explicitly denied the use of location services for this app.
                 self.locationStatusLabel.text = @"Never";
@@ -62,57 +82,79 @@
     
 }
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (indexPath.section ==1 && indexPath.row == 0) {
-        
-        if ([CLLocationManager locationServicesEnabled]){
-            // Location Services Enabled
-            if ([CLLocationManager authorizationStatus]==kCLAuthorizationStatusDenied){
-                // Premission denied.
-                UIAlertController * alert=   [UIAlertController alertControllerWithTitle:@"App Permission Denied"
-                                                                                 message:@"To re-enable, please go to Settings and turn on Location Service for this app."
-                                                                          preferredStyle:UIAlertControllerStyleAlert];
-                UIAlertAction* ok = [UIAlertAction actionWithTitle:@"OK"
-                                                             style:UIAlertActionStyleDefault
-                                                           handler:^(UIAlertAction * action) {
-                                                               [alert dismissViewControllerAnimated:YES completion:nil];
-                                                               [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]];
-                                                           }];
-                UIAlertAction* cancel = [UIAlertAction actionWithTitle:@"Cancel"
-                                                                 style:UIAlertActionStyleCancel
-                                                               handler:^(UIAlertAction * action) {
-                                                                   [alert dismissViewControllerAnimated:YES completion:nil];
-                                                               }];
-                
-                [alert addAction:cancel];
-                [alert addAction:ok];
-                [self presentViewController:alert animated:YES completion:nil];
-            } else {
-                // While using premission.
-                [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]];
-            }
-        } else {
-            // Location Services disabled.
-            UIAlertController * alert=   [UIAlertController alertControllerWithTitle:@"Location Services Disabled"
-                                                                             message:@"To re-enable, please go to \"Settings\"->\"Privacy\"->\"Location Services\" and turn on Location Services."
+
+#pragma mark - Location Services
+
+- (void)setupLocationPremission {
+    if ([CLLocationManager locationServicesEnabled]){
+        // Location Services Enabled
+        if ([CLLocationManager authorizationStatus]==kCLAuthorizationStatusDenied){
+            // Premission denied.
+            UIAlertController * alert=   [UIAlertController alertControllerWithTitle:@"App Permission Denied"
+                                                                             message:@"To re-enable, please go to Settings and turn on Location Service for this app."
                                                                       preferredStyle:UIAlertControllerStyleAlert];
             UIAlertAction* ok = [UIAlertAction actionWithTitle:@"OK"
                                                          style:UIAlertActionStyleDefault
                                                        handler:^(UIAlertAction * action) {
                                                            [alert dismissViewControllerAnimated:YES completion:nil];
-                                                    }];
+                                                           [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]];
+                                                       }];
+            UIAlertAction* cancel = [UIAlertAction actionWithTitle:@"Cancel"
+                                                             style:UIAlertActionStyleCancel
+                                                           handler:^(UIAlertAction * action) {
+                                                               [alert dismissViewControllerAnimated:YES completion:nil];
+                                                           }];
+            
+            [alert addAction:cancel];
             [alert addAction:ok];
             [self presentViewController:alert animated:YES completion:nil];
+        } else {
+            // While using premission.
+            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]];
         }
+    } else {
+        // Location Services disabled.
+        UIAlertController * alert=   [UIAlertController alertControllerWithTitle:@"Location Services Disabled"
+                                                                         message:@"To re-enable, please go to \"Settings\"->\"Privacy\"->\"Location Services\" and turn on Location Services."
+                                                                  preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction* ok = [UIAlertAction actionWithTitle:@"OK"
+                                                     style:UIAlertActionStyleDefault
+                                                   handler:^(UIAlertAction * action) {
+                                                       [alert dismissViewControllerAnimated:YES completion:nil];
+                                                   }];
+        [alert addAction:ok];
+        [self presentViewController:alert animated:YES completion:nil];
     }
-     [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
+
+
+#pragma mark - Mail
+
+- (void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)sendMailWithSubject:(NSString*)subject message:(NSString*)message toRecipient:(NSString*)recipient {
+    if ([MFMailComposeViewController canSendMail]) {
+        MFMailComposeViewController *mail = [[MFMailComposeViewController alloc] init];
+        mail.mailComposeDelegate = self;
+        [mail setSubject:subject];
+        [mail setMessageBody:message isHTML:NO];
+        [mail setToRecipients:@[recipient]];
+        [self presentViewController:mail animated:YES completion:NULL];
+    }
+    else {
+        [self showMessagePrompt:@"This device cannot send email"];
+    }
+}
+
+
 
 //
 //- (void)updateSwitchValueAnimated:(BOOL)animated {
-//    
-//    
-//    
+//
+//
+//
 ////    if ([CLLocationManager locationServicesEnabled] == YES){
 ////        [self.locationSwitch setOn:YES animated:animated];
 ////    } else{

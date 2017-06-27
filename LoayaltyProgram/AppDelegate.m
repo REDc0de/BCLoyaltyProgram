7218a976e379005749cf029b9c3f8c6215767153
Bogdan Chaikovsky <bogdan.chaikovsky@belprog.com>
2017-06-27 10:57:05 +0300

    V 1.0

--- LoayaltyProgram/AppDelegate.m
+++ LoayaltyProgram/AppDelegate.m
@@ -17,14 +17,10 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
     [FIRApp configure];
     [FIRDatabase database].persistenceEnabled = YES;
     
-   // UIColor *promoDarkColor = [UIColor colorWithRed:32.0/255 green:32.0/255 blue:32.0/255 alpha:1.0];
-   // UIColor *facebookLoadColor = [UIColor colorWithRed:237.0/255 green:238.0/255 blue:241.0/255 alpha:1.0]; //hex:EDEEF1
-     UIColor *promoRedColor = [UIColor colorWithRed:219.0/255 green:16.0/255 blue:32.0/255 alpha:1.0];
-
+    UIColor *promoRedColor = [UIColor colorWithRed:219.0/255 green:16.0/255 blue:32.0/255 alpha:1.0];
     [[self window] setTintColor:promoRedColor];
     
     return YES;
